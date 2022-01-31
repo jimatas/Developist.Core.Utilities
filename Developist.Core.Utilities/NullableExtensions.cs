ea5300671cfe5b933914248b1c7f0b1e7d037ef4
jimatas <atask@hr.nl>
2022-01-31 17:36:03 +0100

    NullableExtensions and associated unit tests added. Bumped minor version.

---
+++ Developist.Core.Utilities/NullableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Developist.Core.Utilities
+{
+    public static class NullableExtensions
+    {
+        public static bool IsNullOrDefault<T>(this T? value) where T : struct 
+            => value.GetValueOrDefault().Equals(default(T));
+
+        public static TResult IfNotNull<T, TResult>(this T target, Func<T, TResult> expression)
+            => target != null ? expression(target) : default;
+
+        public static void IfNotNull<T>(this T target, Action<T> expression)
+        {
+            if (target != null)
+            {
+                expression(target);
+            }
+        }
+    }
+}
