d1e32529474c37decb03390a965411b94b4b5e2e
jimatas <atask@hr.nl>
2022-02-05 08:26:38 +0100

    TaskExtensions and AsyncDisposableBase classes added. Now also multi-targeting .NET Standard 2.1. Version bumped.

---
+++ Developist.Core.Utilities/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2022 Jim Atas. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for details.
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Developist.Core.Utilities
+{
+    public static class TaskExtensions
+    {
+        public static ConfiguredTaskAwaitable WithoutCapturingContext(this Task task)
+            => task.ConfigureAwait(continueOnCapturedContext: false);
+
+        public static ConfiguredTaskAwaitable<TResult> WithoutCapturingContext<TResult>(this Task<TResult> task)
+            => task.ConfigureAwait(continueOnCapturedContext: false);
+
+#if NETSTANDARD2_1_OR_GREATER
+        public static ConfiguredValueTaskAwaitable WithoutCapturingContext(this ValueTask task)
+            => task.ConfigureAwait(continueOnCapturedContext: false);
+
+        public static ConfiguredValueTaskAwaitable<TResult> WithoutCapturingContext<TResult>(this ValueTask<TResult> task)
+            => task.ConfigureAwait(continueOnCapturedContext: false);
+#endif
+    }
+}
